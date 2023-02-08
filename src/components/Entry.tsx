31d3b559742b2afff7b804ff16b5674418970423
narinn-star <skfls2618@naver.com>
2023-02-08 19:46:20 +0900

    Entry 위치 값 계산중

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -25,18 +25,35 @@ const Entry = ({ area, data }: EntryProps) => {
   const [isSelected, setIsSelected] = useState(false);
 
   const element = useRef<HTMLDivElement>(null);
+  let top: any, x: any, y: any, bottom: any;
 
   useEffect(() => {
     if (element.current) {
-      const t = element.current.getBoundingClientRect();
-      console.log(t);
+      bottom = element.current.getBoundingClientRect().bottom;
+      top = element.current.getBoundingClientRect().top;
+      x = element.current.getBoundingClientRect().x;
+      y = element.current.getBoundingClientRect().y;
+
+      const size = element.current.getBoundingClientRect();
+      console.log("size : ", size);
+      //console.log("width-top : ", width - top);
     }
   }, [element.current]);
 
+  const sizeCheck = () => {
+    const h = area.end.y - area.start.y; //height
+    const w = area.end.x - area.start.x; //width
+    console.log("H :", h, "W : ", w);
+  };
+
   return (
-    <div className="entry" ref={element} draggable>
-      <FolderIcon />
-    </div>
+    <>
+      <div className="entry" ref={element} draggable>
+        <FolderIcon />
+      </div>
+      {console.log("AreaStart : ", area.start, "AreaEnd", area.end)}
+      {sizeCheck()}
+    </>
   );
 };
 
